c405f393340a99cb41153ba8c6b7921acd67649e
sivanjaneyulupemmasani <sivaanjaneyulupemmasani@gmail.com>
2022-12-23 23:04:46 -0800

    SpringBootDemo-tests

--- spring-boot-seesion/spring-jdbc/src/main/java/com/siva/ems/EmsApplication.java
+++ spring-boot-seesion/spring-jdbc/src/main/java/com/siva/ems/EmsApplication.java
@@ -2,8 +2,7 @@ package com.siva.ems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 
 
